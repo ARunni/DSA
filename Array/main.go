550703844862b9ced4dd6075153c9e7b67578533
Arun C Mohanan <aruncmunni@gmail.com>
2024-01-22 10:17:59 +0530

    remove nearby duplicates

--- Array/main.go
+++ Array/main.go
@@ -1,1 +1,29 @@
+// Remove near by duplicates in an array
 package main
+
+import "fmt"
+
+
+func removeDuplicates(arr []int) int {
+	if len(arr) == 0 {
+		return 0
+	}
+	var newArray []int
+	newArray = append(newArray, arr[0])
+	for i := 1; i < len(arr); i++ {
+		if arr[i] != arr[i-1] {
+			newArray = append(newArray, arr[i])
+		}		
+	}
+	copy(arr,newArray)
+	length := len(newArray)
+	return length
+}
+
+func main()  {
+	arr := []int{1,1,2,2,3,3,4,5}
+	fmt.Println("Before Removing elements",arr)
+	newLength := removeDuplicates(arr)
+	fmt.Println("After",arr[:newLength])
+
+}
