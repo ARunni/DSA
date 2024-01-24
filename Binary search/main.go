adbdc73343bd7f1619aa6b4b640f5c1fd79dd008
Arun C Mohanan <aruncmunni@gmail.com>
2024-01-24 11:06:52 +0530

    updated

---
+++ Binary search/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func binarySearch(arr []int, target int) int {
+	low, high := 0, len(arr)-1
+
+	for low <= high {
+		mid := (low + high) / 2
+
+		if arr[mid] == target {
+			return mid
+		} else if arr[mid] < target {
+			low = mid + 1
+		} else {
+			high = mid - 1
+		}
+	}
+	return -1
+}
+
+func main() {
+	array := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	target := 6
+
+	index := binarySearch(array, target)
+
+	if index != -1 {
+		fmt.Printf("Binary Search: %d found at index %d\n", target, index)
+	} else {
+		fmt.Printf("Binary Search: %d not found in the array\n", target)
+	}
+}
---
+++ Linear Search/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func linearSearch(arr []int, target int) int {
+	for i, value := range arr {
+		if value == target {
+			return i
+		}
+	}
+	return -1
+}
+
+func main() {
+	array := []int{5, 2, 9, 1, 5, 6}
+	target := 1
+
+	index := linearSearch(array, target)
+
+	if index != -1 {
+		fmt.Printf("Linear Search: %d found at index %d\n", target, index)
+	} else {
+		fmt.Printf("Linear Search: %d not found in the array\n", target)
+	}
+}
